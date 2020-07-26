f724ae37363352572b1582299e877c78dbf9baca
Rogo Jagad Alit <rogojagadalit@gmail.com>
2020-07-26 17:11:58 +0700

    Develop (#1)
    * handle create user
    
    * adjust request
    
    * use .env
    
    * handle register
    
    * cleanup
    
    * update env example
    
    * add Procfile

---
+++ src/const/User.js
@@ -0,0 +1,12 @@
+const COLLECTION = "users";
+
+const ATTRIBUTE_BITBUCKET_ID = "bitbucket_id";
+const ATTRIBUTE_CHAT_ID = "chat_id";
+const ATTRIBUTE_NAME = "name";
+
+module.exports = Object.freeze({
+    ATTRIBUTE_BITBUCKET_ID: ATTRIBUTE_BITBUCKET_ID,
+    ATTRIBUTE_CHAT_ID: ATTRIBUTE_CHAT_ID,
+    ATTRIBUTE_NAME: ATTRIBUTE_NAME,
+    COLLECTION: COLLECTION,
+});
--- src/service.js
+++ src/service.js
@@ -1,23 +1,60 @@
-const PLEASE_CHECK_QUERY = "please check";
+const PLEASE_REVIEW_QUERY = "please review";
 
-exports.parseReviewerId = (comment, pullrequest) => {
+const botFactory = require("./bot/factory");
+const messageFactory = require("./message/factory");
+const repository = require("./repository");
+const User = require("./const/User");
+
+exports.parseRequestBody = (comment, pullrequest) => {
     const {
         content: { raw },
         user: { displayName },
     } = comment;
 
     const { reviewers } = pullrequest;
-    const isPleaseCheckRequest = raw.startsWith(PLEASE_CHECK_QUERY);
+    const isPleaseReviewRequest = raw.startsWith(PLEASE_REVIEW_QUERY);
     const reviewerIds = Array();
 
-    if (isPleaseCheckRequest) {
+    if (isPleaseReviewRequest) {
         for (reviewer of reviewers) {
             reviewerIds.push(reviewer.accountId);
         }
     }
 
     return {
+        isReviewRequest: isPleaseReviewRequest,
         pullRequestOwnerName: displayName,
         reviewerIds: reviewerIds,
     };
+};
+
+exports.sendMessageToAllUsers = async (users, pullRequest) => {
+    const botClient = botFactory.getInstance();
+
+    users.forEach((user) => {
+        botClient.sendMessage(
+            user.data()[User.ATTRIBUTE_CHAT_ID],
+            messageFactory.constructMessage(
+                user.data()[User.ATTRIBUTE_NAME],
+                pullRequest
+            )
+        );
+    });
+};
+
+exports.createOneUser = async (chatId, bitbucketId, username) => {
+    const existingData = await repository.readOneUserByBitbucketId(bitbucketId);
+
+    if (existingData !== undefined) {
+        const dataObj = Object();
+
+        dataObj[User.ATTRIBUTE_BITBUCKET_ID] = bitbucketId;
+        dataObj[User.ATTRIBUTE_CHAT_ID] = chatId;
+        dataObj[User.ATTRIBUTE_NAME] = username;
+
+        const result = await repository.createOneUser(dataObj);
+        return result;
+    }
+
+    return existingData;
 };
