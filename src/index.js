26282d40d58a15ba88a545e5b0dc8e1679b1cff2
rogojagad <rogojagadalit@gmail.com>
2020-07-26 17:29:02 +0700

    register webhook handler

--- src/index.js
+++ src/index.js
@@ -8,12 +8,18 @@ const port = process.env.PORT || 5000;
 const bodyParser = require("body-parser");
 const botBootstraper = require("./bot/bootstraper");
 const botFactory = require("./bot/factory");
+const url = process.env.APP_HOST;
+const token = process.env.TELEGRAM_TOKEN;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.disable("etag");
 
 app.post("/callback/bitbucket/reviewer", controller.reviewerAdded);
+app.post(`/bot${token}`, (req, res) => {
+    bot.processUpdate(req.body);
+    res.sendStatus(200);
+});
 
 app.listen(port, (err) => {
     if (err) {
@@ -24,4 +30,5 @@ app.listen(port, (err) => {
 });
 
 const bot = botFactory.createOne();
+bot.setWebHook(`${url}/bot${token}`);
 botBootstraper.bootstrap(bot);
