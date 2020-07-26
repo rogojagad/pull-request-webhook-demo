3c5ed23b352fc21b4013c95f4bb57b8bf60288f2
rogojagad <rogojagadalit@gmail.com>
2020-07-26 17:34:57 +0700

    revise PR link

--- src/message/factory.js
+++ src/message/factory.js
@@ -1,10 +1,11 @@
 exports.constructMessage = (reviewerName, pullRequest) => {
     let body = ``;
+    console.log(pullRequest.links.html);
 
     body += `Hi ${reviewerName},\n\n`;
     body += `Kindly need your review on this PR:\n\n`;
     body += `Title: ${pullRequest.title}\n`;
-    body += `Link: ${pullRequest.links.html}\n\n`;
+    body += `Link: ${pullRequest.links.html.href}\n\n`;
     body += `Thank you`;
 
     return body;
