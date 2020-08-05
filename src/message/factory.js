6426ff750b645c83246f4660447ae49ab2f22c02
rogojagad <rogojagadalit@gmail.com>
2020-08-05 18:21:14 +0700

    revise notif message

--- src/message/factory.js
+++ src/message/factory.js
@@ -3,7 +3,7 @@ exports.constructMessage = (reviewerName, pullRequest) => {
     console.log(pullRequest.links.html);
 
     body += `Hi ${reviewerName},\n\n`;
-    body += `Kindly need your review on this PR:\n\n`;
+    body += `${pullRequest.pullRequestOwnerName} need your review on this following PR:\n\n`;
     body += `Title: ${pullRequest.title}\n`;
     body += `Link: ${pullRequest.links.html.href}\n\n`;
     body += `Thank you`;
